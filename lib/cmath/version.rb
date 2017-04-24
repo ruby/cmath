1084cfc67a43e8c18e3f53d98df975a55e406fe1
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-04-24 15:06:53 +0900

    overrided boilerplate by bundle init cmath

---
+++ lib/cmath/version.rb
@@ -0,0 +1,3 @@
+module Cmath
+  VERSION = "0.1.0"
+end
---
+++ test/cmath_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CmathTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Cmath::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
